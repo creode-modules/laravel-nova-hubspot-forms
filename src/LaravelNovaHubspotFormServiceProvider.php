48c8db44b722bfb686d7f8aa9119134cf071c8bf
Liam Spedding <liam.spedding@creode.co.uk>
2024-01-12 16:55:57 +0000

    Rename service provider

--- src/LaravelNovaHubspotFormServiceProvider.php
+++ src/LaravelNovaHubspotFormServiceProvider.php
@@ -4,7 +4,7 @@ namespace Creode\LaravelNovaHubspotForms;
 
 use Illuminate\Support\ServiceProvider;
 
-class FieldServiceProvider extends ServiceProvider
+class LaravelNovaHubspotFormServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
@@ -29,5 +29,9 @@ class FieldServiceProvider extends ServiceProvider
     public function register()
     {
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'laravel-nova-hubspot-forms');
+
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/laravel-nova-hubspot-forms'),
+        ]);
     }
 }
