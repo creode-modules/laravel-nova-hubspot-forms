b2f688c7205d3e93da3010579f0127b33bf4f384
Liam Spedding <liam.spedding@creode.co.uk>
2024-01-15 09:21:39 +0000

    Add region to component to allow it to be overwritten

--- resources/views/hubspot-form.blade.php
+++ resources/views/hubspot-form.blade.php
@@ -1,1 +1,1 @@
-<x-hubspot-form form-id="{{ $formId }}" portal-id="{{ $portalId }}"></x-hubspot-form>
+<x-hubspot-form form-id="{{ $formId }}" portal-id="{{ $portalId }}" region="{{ $region }}"></x-hubspot-form>
